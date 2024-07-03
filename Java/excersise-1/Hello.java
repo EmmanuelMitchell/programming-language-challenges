4530867a022bb4eaad4d1859075523242a51d1ba
EmmanuelMitchell <emmanuelmitchell1998@gmail.com>
2024-07-03 17:36:08 +0000

    Complet excersise one

--- Java/excersise-1/Hello.java
+++ Java/excersise-1/Hello.java
@@ -1,5 +1,12 @@
+import java.util.Scanner;
+
+
 class Hello{
     public static void main(String[]args){
-        System.out.print("Hello World");
+        Scanner scanner = new Scanner(System.in);  // Create a Scanner object
+    System.out.println("What is your name");
+
+    String userName = scanner.nextLine();  // Read user input
+    System.out.println("Hello " + userName + " " + "nice to meet you!");  // Output user input
     }
 }
